92b05ac88af1bfe4d6069d69d69c750522e30f48
rgdev6 <rgdesenvolvimento@proton.me>
2024-05-18 10:32:27 -0300

    Unused imports removed

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import "package:flutter/material.dart";
 import "package:get_it/get_it.dart";
-import "package:intl/date_symbol_data_local.dart";
 import "package:http/http.dart" as http;
+import "package:intl/date_symbol_data_local.dart";
 
 import "calculator/repository/holidays_repository.dart";
 import "calculator/service/holidays_service.dart";
-import "calculator/service/service.dart";
 import "calculator/view/calculator_page.dart";
 
 void main() {
